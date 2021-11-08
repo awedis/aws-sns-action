d0d7e94599487044a7ff63c9a59aa0df2c7dec7a
Avo-Keofteian <avokeofteian@gmail.com>
2021-11-08 23:44:21 +0200

    chore: try context

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const core = require('@actions/core');
 const AWS = require('aws-sdk');
+const GITHUB = require('@actions/github');
 
 function run() {
   try {
+    const {
+      actor,
+    } = GITHUB.context;
+
     const { getInput } = core;
 
     const accessKeyId = getInput('accessKeyId');
@@ -25,6 +30,7 @@ function run() {
     const SNS = new AWS.SNS();
     const result = SNS.publish(params).promise();
     console.log('result =>', result);
+    console.log('actor =>', actor);
     
   } catch (error) {
     core.setFailed(error.message);
